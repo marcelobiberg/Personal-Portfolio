b933cd4a105f64a4acdd8f27ee812a29177b62d3
marcelo <marcelo_biberg@yahoo.com.br>
2019-01-08 11:00:40 -0200

    First commit Ninja

---
+++ MyOwnBirth/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyOwnBirth.Models;
+
+namespace MyOwnBirth.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Projeto> Projetos { get; set; }
+        public DbSet<ProjetosCategorias> ProjetosCategorias { get; set; }
+    }
+}
